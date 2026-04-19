c0377164925eb3198475b91d48240c3cf475747d
Se Joon Park <sparky9043@gmail.com>
2026-04-19 23:43:53 +0900

    feat: display courses if logged in

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+@login_required
+def courses(request: HttpRequest) -> HttpResponse:
+    return render(request, 'courses/courses.html')
