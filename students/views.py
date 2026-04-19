da7f3e6e3151f59b98499c130d25ea12d805594c
Se Joon Park <sparky9043@gmail.com>
2026-04-19 23:53:07 +0900

    feat: display students if logged in

--- students/views.py
+++ students/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+@login_required
+def students(request: HttpRequest) -> HttpResponse:
+    return render(request, 'students/students.html')
