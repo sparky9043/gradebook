eb31f10e48b1f8b4a49d2a0dfa3f288619843a78
Se Joon Park <sparky9043@gmail.com>
2026-04-20 07:36:03 +0900

    fix: update accounts base url

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('courses/', include('courses.urls')),
     path('students/', include('students.urls')),
 ]
