4ed40dc88bea70d3a6b28cd1a634efe505c3fb19
Se Joon Park <sparky9043@gmail.com>
2026-04-20 12:04:17 +0900

    feat: add student class from person class

--- students/models.py
+++ students/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    
+    class Meta:
+        verbose_name_plural = 'people'
+    
+    def __str__(self):
+        return self.name
+
+class Student(Person):
+    student_id = models.CharField(max_length=50)
+    grade = models.IntegerField(choices=[(9, 9), (10, 10), (11, 11), (12, 12)])
+    
+    def __str__(self):
+        return f"Grade: {self.grade} {self.name}"
